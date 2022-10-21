5f4031534a678961e83068f3dc9ed9545d9b46e1
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-21 02:02:06 +0200

    about section

--- src/components/testmonials/Testmonials.jsx
+++ src/components/testmonials/Testmonials.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './testmonials.css';
 
 const Testmonials = () => {
-  return <div>Testmonials</div>;
+  return <section id="testmonials">Testmonials</section>;
 };
 
 export default Testmonials;
